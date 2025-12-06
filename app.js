f5174671cbc222ebe9fa7686121def0a3c882c19
LG <luigigjurashaj@gmail.comgit config --global user.email luigigjurashaj@gmail.com>
2025-12-06 10:57:00 -0500

    exercise 10

--- app.js
+++ app.js
@@ -216,3 +216,29 @@ for (let object of pokemon) {
     }
 }
 
+//=====================================================================
+
+//                  |---------- Exercise 10 ----------|
+
+
+console.log('\n --- Exercise 10 --- \n');
+/*
+Create a method called `catchPokemon` and add it to the `game` object. You should not need to edit the original game object directly. This method should:
+  - Accept an object as a parameter called `pokemonObj`
+  - Add the `pokemonObj` to the `game.party` array.
+  - not return anything
+
+After writing this method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+
+Solve Exercise 10 here:
+*/
+
+const catchPokemon = {
+function(pokemonObj) {
+    game.party.push({pokemonObj});
+    console.log(game.party);
+}
+};
+catchPokemon.function(pokemon[5]);
+
+
